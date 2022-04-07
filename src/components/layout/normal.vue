a6174343232d8294ba2c4eec8be0635ef9799cf5
Bill-Park <tkddn0916@gmail.com>
2022-04-08 00:49:42 +0900

    사진추가, 버튼 레이아웃 정리, 사진보기 동작 추가

--- src/components/layout/normal.vue
+++ src/components/layout/normal.vue
@@ -1,31 +1,37 @@
 <template>
-    <v-app>
-        <v-main>
-            <v-container>
-                normal
-                메인사진
-            </v-container>
-            <v-btn @click="openSubmit">참석 등록</v-btn>
-            <v-btn @click="openSubmit">사진보기</v-btn>
-            <v-btn @click="openSubmit">약도보기</v-btn>
-            <layout-attendance
-            v-show="submitFlag"
-            @submitClose="submitClose"/>
-        </v-main>
-    </v-app>
+  <v-app>
+    <v-main>
+      <v-container class="d-flex flex-column">
+        <v-img src="@/assets/main.jpg" height="50%"> </v-img>
+        <v-row group class="mt-4 align-self-center">
+          <v-btn outlined x-large class="rounded rounded-r-0 title" @click="openSubmit">참석 등록</v-btn>
+          <v-btn outlined x-large class="rounded-0 title" @click="openPhotos">사진보기</v-btn>
+          <v-btn outlined x-large class="rounded rounded-l-0 title" @click="openSubmit">약도보기</v-btn>
+        </v-row>
+        <v-row class="align-self-center red--text lighten-1" v-show="!submitFlag">
+          참석 등록을 하지않은 경우 식사가 어려울 수 있으니 꼭 등록해주시기
+          바랍니다.
+        </v-row>
+        <layout-attendance class="mt-5" v-show="submitFlag" @submitClose="submitClose" />
+        <layout-photos v-if="photoFlag" @photoClose="photoCLose"/>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
 import LayoutAttendance from '@/components/layout/attendance.vue'
+import LayoutPhotos from '@/components/layout/photos.vue'
 
 @Component({
   components: {
-    LayoutAttendance
+    LayoutAttendance, LayoutPhotos
   }
 })
 export default class LayoutNormal extends Vue {
     submitFlag = false
+    photoFlag = false
 
     openSubmit (): void {
       this.submitFlag = true
@@ -33,6 +39,14 @@ export default class LayoutNormal extends Vue {
 
     submitClose (): void {
       this.submitFlag = false
+    }
+
+    openPhotos (): void{
+      this.photoFlag = true
+    }
+
+    closePhotos (): void{
+      this.photoFlag = false
     }
 }
 </script>
---
+++ src/components/layout/photos.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app>
+    <v-main>
+      웨딩사진 보기
+      <v-container class="d-flex flex-column justify-center">
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class LayoutPhotos extends Vue {
+  closePhoto ():void { // close photo page
+    this.$emit('photoClose')
+  }
+}
+
+</script>
+
+<style scoped>
+
+</style>
